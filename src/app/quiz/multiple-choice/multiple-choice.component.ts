0091cd181b636f4b4c8ff1580503e9bd561c5f98
adii <abhishekdass08@gmail.com>
2017-12-16 16:12:19 -0500

    update files

--- src/app/quiz/multiple-choice/multiple-choice.component.ts
+++ src/app/quiz/multiple-choice/multiple-choice.component.ts
@@ -17,6 +17,9 @@ export class MultipleChoiceComponent implements OnInit {
   @Input() headerSectionFormGroup: FormGroup;
   @Input() fieldName: string;
   @Input() qz: any;
+  @Input() index;
+  @Input() isLastQ;
+  @Input() navigateNext;
   
   public istest: boolean;
   public checkedCount: number;
@@ -65,7 +68,6 @@ export class MultipleChoiceComponent implements OnInit {
     }
   }
 
-  
   testfn(navId) {
     
     if(!this.headerSectionFormGroup.controls[this.fieldName].valid) {
